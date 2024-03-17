9e0e4ec8e9ff3ef04f26a5ed4d082098846991fd
ethanwke <158763809+ethanwke@users.noreply.github.com>
2024-03-17 21:47:55 +0800

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,15 @@
+<?php
+  $name = $_POST['name'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <h2>Login Successful</h2><br>
+  <h1>Welcome, <?php echo $name ?></h1>
+</body>
+</html>
