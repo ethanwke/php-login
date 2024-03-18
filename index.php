e704a7126cdcca5d9b7a529d78caa484e7a23029
ethanwke <158763809+ethanwke@users.noreply.github.com>
2024-03-19 00:28:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,54 @@
+<?php
+if(isset($_COOKIE['loggedIn'])){
+    if ($_COOKIE['loggedIn'] == true){
+        header("Location: dashboard.php");
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="index.js" defer></script>
 </head>
 <body>
   <form action="index.php" method="post">
     <label for="name">Name:</label>
-    <input type="text" name="name" id="name"><br><br>
+    <input type="text" name="name" id="name"><br>
     <label for="password">Password:</label>
-    <input type="password" name="password" id="password"><br><br>
-    <input type="submit" name="submit" value="Submit">
+    <input type="password" name="password" id="password">
+    <input type="submit" name="submit" value="Submit" style="margin: 20px 0;">
   </form>
+  <button id="btn_showPassword" onclick="showText('password')">Show Password</button><br>
   <?php
     if(isset($_POST['submit'])){
       $name = $_POST['name'];
       $password = $_POST['password'];
       
-      $data_name = array("ethan", "xuan hao");
-      $data_password = array("ethan", "xuan hao");
 
-
-      if(in_array($name, $data_name) && in_array($password, $data_password)){
+      $name_password = array("ethan"=>"ethan", "xuan hao"=>"xuan hao");
+      if($password == $name_password[$name]){
         echo "Login Successful";
-        echo "
-        <form id='autosubmit' action='dashboard.php' method='post'>
-        <input type='hidden' name='name' value='{$name}'>
-        </form>
-        ";
-        echo "
-        <script type='text/javascript'>
-          document.getElementById('autosubmit').submit();
-        </script>";
+        setcookie("name", $name, time() + 86400, "/"); // expires in one day
+        setcookie("password", $password, time() + 86400);
+        setcookie("loggedIn", true, time() + 86400, "/"); // log in status is valid for a day
         echo "<script>window.location.href='dashboard.php';</script>";
-        
       }
       else{
-        echo "Login Failed";
+        echo "<h2 style='color: red;'>Login Failed</h2><br>Please check your username and password.";
       }
     }
   ?>
   <p>Please Login</p>
+  <h3>New Feature:</h3>
+  <ul>
+    <li>You stays logged in for 1 day every time you Login, unless you logged out.</li>
+    <li>When you stays logged in, opening <a href="index.php">index.php</a> will redirect you to dashboard.</li>
+    <li>Source code: <a href="https://github.com/ethanwke/php-login" target="_blank">Click Me.</a></li>
+  </ul>
+  <p>Programmed by Wong Khai Ee.</p>
+
 </body>
 </html>
