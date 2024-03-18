8205a75dfd28fcb7ad278022baecd833866e9c2b
ethanwke <158763809+ethanwke@users.noreply.github.com>
2024-03-19 00:27:42 +0800

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,10 +1,12 @@
 <?php
-  $name = $_POST['name'];
-  $data_name = array("ethan", "xuan hao");
-  if(!in_array($name,$data_name)){
-     echo "<script>window.location.href='index.php';</script>";
+  $name = $_COOKIE['name'];
+  $password = $_COOKIE['password'];
+  $name_password = array("ethan"=>"ethan", "xuan hao"=>"xuan hao");
+  if ($password != $name_password[$name]){
+    header("Location: index.php");
   }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +15,19 @@
     <title>Document</title>
 </head>
 <body>
-  <h2>Login Successful</h2><br>
-  <h1>Welcome, <?php echo $name ?></h1>
+  <h2 style="color:lime;">Login Successful</h2><br>
+  <h1>Welcome, <?php echo ucwords($name)?></h1>
+  <form action="dashboard.php" method="get">
+    <button type="submit" name="Logout" value="1">Logout?</button>
+  </form>
+  <?php
+  
+  if(isset($_GET["Logout"])) {
+    setcookie ("name","",time() -3600,"/");
+    setcookie ("password","",time() -3600,"/");
+    setcookie ("loggedIn","",time() -3600,"/");
+    header('Location: index.php');
+  }
+  ?>
 </body>
 </html>
